dfe6afba49209bd15da9a75b792b5be8478d4092
Alex <1080810@alumno.um.edu.mx>
2021-09-07 15:50:59 -0500

    Implementing poster prefab

---
+++ UMCampusVirtual/Assets/Scripts/Stands/poster/trigger_behaviour.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using StarterAssets;
+
+public class trigger_behaviour : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject button;
+    public Image image;
+    public bool isIn = false;
+
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("PlayerArmature");
+    }
+
+    private void Update()
+    {
+     
+        if (isIn && Input.GetKeyDown(KeyCode.E))
+        {
+            showPoster(true);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            showUI(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            showUI(false);
+        }
+    }
+
+    private void showUI(bool value)
+    {
+        isIn = value;
+        text.SetActive(value);
+    }
+
+    public void showPoster(bool value)
+    {
+        text.SetActive(!value);
+        image.enabled = value;
+        button.SetActive(value);
+        player.GetComponent<ThirdPersonController>().enabled = !value;
+        Cursor.lockState = value? CursorLockMode.None:CursorLockMode.Locked;
+    }
+
+}
